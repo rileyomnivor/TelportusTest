518f4ee901a99162b1d8924e5c306634a6d0c481
rileyomnivor <66850730+rileyomnivor@users.noreply.github.com>
2020-07-15 10:50:06 -0700

    Add files via upload

---
+++ app/src/main/java/io/antmedia/android/MainActivity.java
@@ -0,0 +1,33 @@
+package io.antmedia.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import io.antmedia.android.liveVideoBroadcaster.*;
+import io.antmedia.android.liveVideoPlayer.LiveVideoPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * PLEASE WRITE RTMP BASE URL of the your RTMP SERVER.
+     */
+    public static final String RTMP_BASE_URL = "rtmp://192.168.1.3/LiveApp/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(io.antmedia.android.liveVideoBroadcaster.R.layout.activity_main);
+    }
+
+    public void openVideoBroadcaster(View view) {
+        Intent i = new Intent(this, LiveVideoBroadcasterActivity.class);
+        startActivity(i);
+    }
+
+    public void openVideoPlayer(View view) {
+        Intent i = new Intent(this, LiveVideoPlayerActivity.class);
+        startActivity(i);
+    }
+}
